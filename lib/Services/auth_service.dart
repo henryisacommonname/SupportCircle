a0995550b277a456efdabf80d07b75f24401212e
Henry Cheng <chenghenry22@gmail.com>
2025-07-24 08:40:53 +0800

    setting up auth services

---
+++ lib/Services/auth_service.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+class AuthService {
+  final _auth=FirebaseAuth.instance;
+      Stream<User?> get userChanges => _auth.authStateChanges();
+
+      Future<UserCredential> registerWithEmail({required String email,required String password,}){
+        return _auth.createUserWithEmailAndPassword(email: email, password: password);
+      }
+    Future<UserCredential> signInWithEmail({required String email,required String password,}){
+      return _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+  Future<void> signOut()=> _auth.signOut();
+}
