e193e0e81e038cc834007574bb78eaf70ec854fd
Henry Cheng <chenghenry22@gmail.com>
2025-07-30 12:01:32 +0800

    login scren WIP firebase auth erorr

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,18 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 
 class loginscreen extends StatefulWidget {
   // final VoidCallback onRegisterTap;
   // TODO const LoginScreen ({required this.onRegisterTap});
-
+  final VoidCallback onRegisterTap;
+  const loginscreen({Key? key,required this.onRegisterTap}) : super(key: key);
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<loginscreen> {
   final _auth = AuthService();
-  final _emailCtrl = TextEditingController();
-  final _passCtrl = TextEditingController();
+  final TextEditingController _emailCtrl = TextEditingController();
+  final TextEditingController _passCtrl = TextEditingController();
+  String? _errormessage;
+  bool _isloading=false;
+
+  Future<void> _sign_in() async {
+    setState(() {
+      _isloading=true;
+      _errormessage=null;
+    });
+
+    try {
+      await _auth.signInWithEmail(
+        email: _emailCtrl.text.trim(),
+        password: _passCtrl.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() => _errormessage = e.message);
+    }
+  }
 
   @override
   Widget build(BuildContext ctx) => Scaffold(
@@ -23,13 +43,32 @@ class _LoginScreenState extends State<loginscreen> {
         children: [
           TextField(
             controller: _emailCtrl,
-            decoration: const InputDecoration(labelText: "Email Address"),
+            decoration: const InputDecoration(
+              labelText: "Email Address",
+              prefixIcon: Icon(Icons.email),
+            ),
           ),
           TextField(
             controller: _passCtrl,
-            decoration: const InputDecoration(labelText: "Password"),
+            decoration: const InputDecoration(
+              labelText: "Password",
+              prefixIcon: Icon(Icons.lock),
+            ),
           ),
+          const SizedBox(height: 12),
+          SizedBox(
+              width:12,
+              //TODO sign-in
+              child: ElevatedButton(
+                  onPressed: _sign_in,
+                  child: const Text("Sign In"))
+          ),
+          TextButton(
+              onPressed: widget.onRegisterTap,
+              child: const Text("Register"))
         ],
+
+        //sign-in button
       ),
     ),
   );
