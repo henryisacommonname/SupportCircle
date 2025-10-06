97955d68d6c42af11feee51dc0839d7c37891d56
Henry Cheng <chenghenry22@gmail.com>
2025-10-05 22:17:08 -0400

    Module Player set-up

---
+++ lib/Screens/Module_Player.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../Core/Training_Repository.dart';
+import 'package:video_player/video_player.dart';
+
+class ModulePlayerScreen extends StatefulWidget {
+  final TrainingModule Trainingscreen;
+  const ModulePlayerScreen({super.key, required this.Trainingscreen});
+  @override
+  State<ModulePlayerScreen> createState() => ModulePlayerScreenState();
+}
+
+class ModulePlayerScreenState extends State<ModulePlayerScreen> {
+  late final TrainingRepository Repo;
+  bool loadingVideo = false;
+
+  // VideoPlayerController? _controller;
+  @override
+  Widget build(BuildContext context) {
+    final m = widget.Trainingscreen;
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(title: Text(m.title)),
+      body: ListView(padding: const EdgeInsets.all(16), children: []),
+    );
+  }
+}
+
+class VideoSection extends StatelessWidget {
+  final VideoPlayerController? VideoController;
+  final bool IsLoading;
+  const VideoSection({required this.VideoController, required this.IsLoading});
+
+  @override
+  Widget build(BuildContext context) {
+    if (IsLoading) {
+      return AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Center(child: CircularProgressIndicator()),
+      );
+    }
+    if (VideoController == null || !VideoController!.value.isInitialized) {
+      return AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Center(child: Icon(Icons.play_disabled)),
+      );
+    }
+    ;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: AspectRatio(
+        aspectRatio: VideoController!.value.aspectRatio == 0
+            ? (16 / 9)
+            : VideoController!.value.aspectRatio,
+        child: VideoPlayer(VideoController!),
+      ),
+    );
+  }
+}
+
+class ArticleText extends StatelessWidget {
+  final String? Subtitle;
+  final String? BodyText;
+
+  const ArticleText({required this.Subtitle, required this.BodyText});
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(Subtitle!, style: theme.textTheme.titleMedium),
+        const SizedBox(height: 10),
+        Text(BodyText!.trim()),
+      ],
+    );
+  }
+}
